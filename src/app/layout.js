f84eb96296acaa0f4258d940b440c034f937d654
joahyi <joahyi@qq.com>
2023-03-18 10:20:31 +0800

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "./components/Header";
 
 export const metadata = {
   title: "IMDb Clone",
@@ -8,7 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
   );
 }
